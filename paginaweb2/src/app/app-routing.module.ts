61936cc56690d51fba13c8d8cd74f8c6fbc80f66
AndresS <andres.solanov.est@uets.edu.ec>
2022-12-05 12:36:18 -0500

    tercer commit

---
+++ paginaweb2/src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { Componente1Component } from './componentes/componente1/componente1.component';
+
+const routes: Routes = [
+  {
+    path: '', component: Componente1Component
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- paginaweb2/src/app/componentes/componente1/componente1.component.ts
+++ paginaweb2/src/app/componentes/componente1/componente1.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-componente1',
   templateUrl: './componente1.component.html',
   styleUrls: ['./componente1.component.css']
 })
-export class Componente1Component {
+export class Componente1Component implements OnInit{
+
+  constructor(){}
+  numero: any=0;
+
+  ngOnInit():void{
+    this.numero
+  }
 
 }
